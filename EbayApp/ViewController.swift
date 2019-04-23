c241f8e9ed2f332ab0b28646bcc368f407aba0fb
Deyi Kong <deyikong@yahoo.com>
2019-04-23 00:45:37 -0500

    Initial Commit

---
+++ EbayApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EbayApp
+//
+//  Created by Deyi Kong on 4/23/19.
+//  Copyright © 2019 Deyi Kong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
